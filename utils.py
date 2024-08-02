c58f78fe9e81e47491a3e5c21824514d0d7090ed
the2roock <maksympetrynko68@gmail.com>
2024-08-02 14:06:09 +0300

    Refactor code

--- utils.py
+++ utils.py
@@ -2,6 +2,14 @@ from undetected_chromedriver import Chrome, ChromeOptions
 
 
 def create_driver() -> Chrome:
+    """Run a browser driver
+
+    Returns:
+        Chrome: Browser driver interface
+    """
     options = ChromeOptions()
+    
+    options.add_argument("--headless")  # Run Chrome in headless mode (without a GUI)
+    
     chrome = Chrome(options=options)
     return chrome
